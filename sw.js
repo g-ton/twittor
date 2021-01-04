aea5a0931bb670ca11da92c17ddee5fcba40b75a
J. Damián <bionik_gotik@hotmail.com>
2021-01-04 10:57:23 -0600

    Cambios en sw y css para ios

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('/twittor/js/sw-utils.js');
 const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -34,6 +34,10 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
         keys.forEach(key => {
             if(key !== STATIC_CACHE && key.includes('static')){ //Cuando cambiamos la versión de la caché estática, comprobamos para borrar la versión antigua
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
         });
